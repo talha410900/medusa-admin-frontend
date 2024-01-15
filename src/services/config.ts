6d1cdda027a2dc44b656cb8dcccc8341849077ae
appledefender <appledefender@gmail.com>
2024-01-15 20:41:54 +0500

    changes

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,3 +1,3 @@
-const medusaUrl = __MEDUSA_BACKEND_URL__ || "https://medusa-admin-production-25fd.up.railway.app"
+const medusaUrl = __MEDUSA_BACKEND_URL__ || "https://medusa-admin-production-3cc9.up.railway.app"
 
 export { medusaUrl }
